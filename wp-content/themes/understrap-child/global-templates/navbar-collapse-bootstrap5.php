6d0f113ca41afe88fb1fde02f348dc204b6b6b92
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-10-15 14:50:08 +0530

    changes

--- wp-content/themes/understrap-child/global-templates/navbar-collapse-bootstrap5.php
+++ wp-content/themes/understrap-child/global-templates/navbar-collapse-bootstrap5.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Header Navbar (bootstrap5)
  *
@@ -7,51 +8,45 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
+<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-dark" aria-labelledby="main-nav-label">
 
-	<h2 id="main-nav-label" class="screen-reader-text">
-		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-	</h2>
+    <h2 id="main-nav-label" class="screen-reader-text">
+        <?php esc_html_e('Main Navigation', 'understrap'); ?>
+    </h2>
 
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+    <div class="<?php echo esc_attr($container); ?>">
 
-		<!-- Your site branding in the menu -->
-		<?php get_template_part( 'global-templates/navbar-branding' ); ?>
+        <!-- Your site branding in the menu -->
+        <?php get_template_part('global-templates/navbar-branding'); ?>
 
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarNavDropdown"
-			aria-controls="navbarNavDropdown"
-			aria-expanded="false"
-			aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>"
-		>
-			<span class="navbar-toggler-icon"></span>
-		</button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false"
+            aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-		<!-- The WordPress Menu goes here -->
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => 'primary',
-				'container_class' => 'collapse navbar-collapse',
-				'container_id'    => 'navbarNavDropdown',
-				'menu_class'      => 'navbar-nav ms-auto',
-				'fallback_cb'     => '',
-				'menu_id'         => 'main-menu',
-				'depth'           => 2,
-				'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-			)
-		);
-		?>
+        <!-- The WordPress Menu goes here -->
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location'  => 'primary',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id'    => 'navbarNavDropdown',
+                'menu_class'      => 'navbar-nav ms-auto',
+                'fallback_cb'     => '',
+                'menu_id'         => 'main-menu',
+                'depth'           => 2,
+                'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+            )
+        );
+        ?>
 
-	</div><!-- .container(-fluid) -->
+    </div><!-- .container(-fluid) -->
 
 </nav><!-- #main-nav -->
---
+++ wp-content/themes/understrap-child/home-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Template Name: Home Page
+ * Template Post Type: page
+ */
+get_header();
+?>
+
+<?php
+the_content();
+?>
+<?php get_footer(); ?>
