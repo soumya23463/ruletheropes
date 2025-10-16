305bd05f0458efacf36488dced5f9c5a3d1727e9
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-10-16 11:45:26 +0530

    changes

--- wp-content/themes/understrap-child/global-templates/navbar-branding.php
+++ wp-content/themes/understrap-child/global-templates/navbar-branding.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Navbar branding
  *
@@ -7,27 +8,42 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! has_custom_logo() ) { ?>
+if (! has_custom_logo()) { ?>
 
-	<?php if ( is_front_page() && is_home() ) : ?>
+<?php if (is_front_page() && is_home()) : ?>
 
-		<h1 class="navbar-brand mb-0">
-			<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-				<?php bloginfo( 'name' ); ?>
-			</a>
-		</h1>
+<h1 class="navbar-brand mb-0">
+    <a rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url">
+        <?php bloginfo('name'); ?>
+    </a>
+</h1>
 
-	<?php else : ?>
+<?php else : ?>
 
-		<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-			<?php bloginfo( 'name' ); ?>
-		</a>
+<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" itemprop="url">
+    <?php bloginfo('name'); ?>
+</a>
 
-	<?php endif; ?>
+<?php endif; ?>
 
-	<?php
+<?php
 } else {
-	the_custom_logo();
+?>
+<div class="custom-logo-wrapper" style="height: 100px;">
+    <?php the_custom_logo(); ?>
+</div>
+<style>
+/* .custom-logo-wrapper .custom-logo-link {
+    display: inline-block;
+    height: 100px;
+} */
+
+.custom-logo-wrapper .custom-logo-link img {
+    height: 85px;
+    width: auto;
+}
+</style>
+<?php
 }
